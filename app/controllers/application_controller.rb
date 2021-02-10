5077c58336309ad90f5904c7539a3e0791df7c59
Peter Jang <peter.x.jang@gmail.com>
2021-02-09 20:44:04 -0600

    Update protect from forgery for consistency

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception, if: -> { request.format.html? }
 
   def current_user
-    if request.headers['Authorization'].present?
-      token = request.headers['Authorization'].split(' ').last
+    if request.headers["Authorization"].present?
+      token = request.headers["Authorization"].split(" ").last
       begin
         decoded_token = JWT.decode(
           token,
           "random",
           true,
-          { algorithm: 'HS256' }
+          { algorithm: "HS256" }
         )
         User.find_by(id: decoded_token[0]["user"])
       rescue JWT::ExpiredSignature
